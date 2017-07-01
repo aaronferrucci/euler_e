6580b4a9b0269ebaaa03b46f8abe21c3be3f8f14
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2017-07-01 10:44:53 -0700

    Clean up some comments; add github link.

--- app.R
+++ app.R
@@ -1,41 +1,30 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 library(ggplot2)
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-   
    # Application title
    titlePanel("Approximating e with Euler's Algorithm"),
    
-   # Sidebar with a slider input for number of bins 
+   # Sidebar with a slider input for number of steps 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
+        sliderInput("steps",
                      "Number of steps:",
                      min = 1,
                      max = 50,
                      value = 10)
       ),
-      
-      # Show a plot of the generated distribution
+      # Show the plot
       mainPanel(
-         plotOutput("distPlot")
+        plotOutput("distPlot"),
+        a("source code on github", href="https://github.com/aaronferrucci/euler_e", target="_blank")
       )
    )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
    output$distPlot <- renderPlot({
-     n <- input$bins
+     n <- input$steps
      delta <- 1/n
      f <- numeric(n + 1)
      f[1] <- 1
